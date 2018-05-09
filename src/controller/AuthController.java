b9d0c3ba879f859c02b2f382a7d46929e4ec99aa
Nguyen Van Phu <nguyenphu14@gmail.com>
2018-05-09 11:24:19 +0700

    abc

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import constant.Defines;
+import constant.Defines;//aa
 
 @Controller
 @RequestMapping("auth")
